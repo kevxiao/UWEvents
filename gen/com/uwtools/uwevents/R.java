878a9ae038b71863502ddace171de7c118d59800
KevXman <kevinxiao2@gmail.com>
2014-01-11 17:46:20 -0500

    Drawer

--- gen/com/uwtools/uwevents/R.java
+++ gen/com/uwtools/uwevents/R.java
@@ -21,14 +21,21 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int header=0x7f020000;
-        public static final int header_background=0x7f020001;
-        public static final int ic_action_refresh=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
+        public static final int drawer_shadow=0x7f020000;
+        public static final int header=0x7f020001;
+        public static final int header_background=0x7f020002;
+        public static final int ic_action_refresh=0x7f020003;
+        public static final int ic_drawer=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int rss=0x7f020006;
     }
     public static final class id {
-        public static final int action_refresh=0x7f080001;
-        public static final int action_settings=0x7f080000;
+        public static final int action_refresh=0x7f080005;
+        public static final int action_settings=0x7f080004;
+        public static final int drawer_layout=0x7f080000;
+        public static final int layoutHeader=0x7f080002;
+        public static final int left_drawer=0x7f080001;
+        public static final int logo=0x7f080003;
     }
     public static final class layout {
         public static final int activity_events=0x7f030000;
@@ -40,6 +47,8 @@ public final class R {
         public static final int action_refresh=0x7f050004;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int drawer_close=0x7f050006;
+        public static final int drawer_open=0x7f050005;
         public static final int hello_world=0x7f050002;
         public static final int loading=0x7f050003;
     }
--- src/com/uwtools/uwevents/Events.java
+++ src/com/uwtools/uwevents/Events.java
@@ -29,6 +29,11 @@ import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.content.res.Configuration;
+import android.view.MenuItem;
+
 public class Events extends Activity {
 	
 	// Progress Dialog
@@ -50,6 +55,11 @@ public class Events extends Activity {
     public static String TAG_ID = "id";
     public static String TAG_TITLE = "title";
     public static String TAG_LINK = "link";
+    
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+    private CharSequence mDrawerTitle;
+    private CharSequence mTitle;
      
     // List view
     ListView lv;
@@ -58,8 +68,63 @@ public class Events extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mTitle = mDrawerTitle = getTitle();
+        // enable ActionBar app icon to behave as action to toggle nav drawer
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+
+        // ActionBarDrawerToggle ties together the the proper interactions
+        // between the sliding drawer and the action bar app icon
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,                   //host Activity 
+                mDrawerLayout,          //DrawerLayout object 
+                R.drawable.ic_drawer,   //nav drawer image to replace 'Up' caret 
+                R.string.drawer_open,   //"open drawer" description for accessibility 
+                R.string.drawer_close   //"close drawer" description for accessibility 
+                ) {
+            public void onDrawerClosed(View view) {
+                getActionBar().setTitle(mTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                getActionBar().setTitle(mDrawerTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+        
+        // Set the drawer toggle as the DrawerListener
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+    }
+    
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
     }
 
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+          return true;
+        }
+        // Handle your other action bar items...
+
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
